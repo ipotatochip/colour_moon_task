2c7f86062ce79714b2e1560998d12dedfda68735
ipotatochip <akhileswarpati333@gmail.com>
2023-02-07 00:24:19 +0530

    images extention changed

--- js/script.js
+++ js/script.js
@@ -38,17 +38,17 @@ $(document).ready(function(){
 
   $(document).ready(function(){
     $('.img-hvr-1').hover(function(){
-    $(this).attr('src','./images/rest_images/after_1.png');
+    $(this).attr('src','./images/rest_images/after_1.webp');
     },function(){
-    $(this).attr('src','./images/rest_images/before_1.png');
+    $(this).attr('src','./images/rest_images/before_1.webp');
     });
     });
 
     $(document).ready(function(){
       $('.img-hvr-2').hover(function(){
-      $(this).attr('src','./images/rest_images/after_2.png');
+      $(this).attr('src','./images/rest_images/after_2.webp');
       },function(){
-      $(this).attr('src','./images/rest_images/before_2.png');
+      $(this).attr('src','./images/rest_images/before_2.webp');
       });
       });
 
